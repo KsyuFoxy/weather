139a7f2b99a43e5e312d69e4818edf33654f47ed
Oksana Kondratiuk <ksyu@web.de>
2017-06-21 17:47:58 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { ColorSpectrumDirective } from './colors';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Weather in your City!';
+  // cityName: string;
+  data: any;
+  cityName = 'London,uk';
+  weatherLink: string;
+  weather = false;
+  inputValue;
+  @Input() input: string;
+  @Output() inputChange = new EventEmitter();
+
+  weatherImag = './images/SunCloud.png';
+
+// constructor(http: Http) {
+//     http.get('//api.openweathermap.org/data/2.5/weather?q=' + this.cityName + '&appid=2e24ce1a5691ac298a7d48bb0d69efc9').subscribe( response => {
+//       this.data = response.json();
+//         console.log('data', this.data);
+//   });
+// }
+
+    changeInput(newInput) {
+        this.input = newInput;
+        this.inputChange.emit(newInput);
+        if (newInput == '') {
+            this.closeWeather();
+        }
+    }
+  getWeather(input: string) {
+      if (input) {
+          this.cityName = input;
+          this.weatherLink = '//api.openweathermap.org/data/2.5/weather?q=' + this.cityName + '&appid=2e24ce1a5691ac298a7d48bb0d69efc9';
+          console.log('cityName', this.cityName);
+          this.weather = true;
+          this.inputValue = input;
+      } else if (input = ' ') {
+          console.log('nothing choosen');
+      }
+  }
+  closeWeather() {
+      this.weather = false;
+      this.inputValue = '';
+  }
+
+
+}
