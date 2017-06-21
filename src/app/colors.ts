c8fc6771bf19fd995891a32549c6e67341d00ec8
Oksana Kondratiuk <ksyu@web.de>
2017-06-21 17:57:33 +0200

    change colors and even (click)

--- src/app/colors.ts
+++ src/app/colors.ts
@@ -12,35 +12,12 @@ export class ColorSpectrumDirective {
     constructor(private el:ElementRef,private render:Renderer){
      }
 
-     @HostListener('mouseenter') methodToHandleMouseEnterAction(){
+     @HostListener('click') methodToHandleMouseEnterAction(){
          console.log(this.highlightColor);
          this.changecolor(this.highlightColor || this._defaulColor);
-    }
-
-     @HostListener('mouseleave') methodToHandleMouseExitAction(){
-         console.log(this.highlightColor);
-         this.changecolor(null);
     }
 
       private changecolor(color: string) {
          this.render.setElementStyle(this.el.nativeElement, 'background-color', color);
     }
 }
-
-// @Component({
-//   selector: 'my-tasks',
-//   template: `
-//     <div (click)="color='#ec6a96'" [ngStyle]="{'background-color': '#ec6a96'}"></div>
-//     <div (click)="color='#6262ea'" [ngStyle]="{'background-color': '#6262ea'}"></div>
-//     <div (click)="color='yellow'" [ngStyle]="{'background-color': 'yellow'}"></div>
-//     <div (click)="color='#5ff45f'" [ngStyle]="{'background-color': '#5ff45f'}"></div>
-//     <div (click)="color='#fb60fb'" [ngStyle]="{'background-color': '#fb60fb'}"></div>
-//
-//     <p [color-spectrum]="color">I like this color</p>
-//   `,
-//   styleUrls: [ './color.component.css' ]
-// })
-// export class ColorsComponent {
-//
-// }
-// https://www.infragistics.com/community/blogs/dhananjay_kumar/archive/2016/11/01/what-is-attribute-directives-in-angular-2.aspx
