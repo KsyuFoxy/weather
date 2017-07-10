5af161fb468debf952d9a4124f543cc1900daa46
Oksana Kondratiuk <ksyu@web.de>
2017-07-10 17:09:15 +0200

    add Forecast & responsive design
    usein JSON file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ axios.defaults.withCredentials = true;
 export class AppComponent {
   title = 'Weather in your City!';
   cityName: string;
-  data: any;
+  // data: any;
   weatherLink: string;
   weather = false;
   inputValue;
@@ -23,18 +23,23 @@ export class AppComponent {
   temp;
   weatherImag;
 
-  // data = {"coord":{"lon":13.41,"lat":52.52},
-  //           "weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],
-  //           "base":"stations",
-  //           "main":{"temp":20.49,"pressure":1008,"humidity":64,"temp_min":20,"temp_max":21},
-  //           "visibility":10000,
-  //           "wind":{"speed":2.1,"deg":70},
-  //           "clouds":{"all":0},
-  //           "dt":1499671200,
-  //           "sys":{"type":1,"id":4892,"message":0.0021,"country":"DE","sunrise":1499655377,"sunset":1499714801},
-  //           "id":2950159,
-  //           "name":"Berlin",
-  //           "cod":200}
+  data = {"coord":{"lon":13.41,"lat":52.52},
+            "weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],
+            "base":"stations",
+            "main":{"temp":20.49,"pressure":1008,"humidity":64,"temp_min":20,"temp_max":21},
+            "visibility":10000,
+            "wind":{"speed":2.1,"deg":70},
+            "clouds":{"all":0},
+            "dt":1499671200,
+            "sys":{"type":1,"id":4892,"message":0.0021,"country":"DE","sunrise":1499655377,"sunset":1499714801},
+            "id":2950159,
+            "name":"Berlin",
+            "cod":200}
+forecast: any;
+showForecast = true;
+fDay;
+fDays = [];
+forecustButton = 'Show Forecast';
 
 
 constructor(http: Http) {
@@ -48,30 +53,17 @@ constructor(http: Http) {
             this.closeWeather();
         }
     }
-  getWeather(input: string) {
-      if (input) {
-          this.cityName = input;
-          const options = new RequestOptions({withCredentials: true});
-        //   this.http.get('http://api.openweathermap.org/data/2.5/weather?q=' + this.cityName + '&appid=2e24ce1a5691ac298a7d48bb0d69efc9', options).subscribe( response => {
-          this.http.get('http://ec2-52-59-160-108.eu-central-1.compute.amazonaws.com/api/single-weather?city=' + this.cityName, options).subscribe( response => {
-            this.data = response.json();
-            console.log('data', this.data);
-            this.weatherImag = './src/app/images/'+ this.data.weather[0].icon + '.png';
-            this.temp = this.data.main.temp;
-        });
-          this.weather = true;
-          this.inputValue = input;
-      } else if (input = ' ') {
-          console.log('nothing choosen');
-      }
-  }
-
   // getWeather(input: string) {
   //     if (input) {
   //         this.cityName = input;
-  //         this.weatherImag = './src/app/images/'+ this.data.weather[0].icon + '.png';
-  //         this.temp = this.data.main.temp;
-  //         console.log('data', this.data);
+  //         const options = new RequestOptions({withCredentials: true});
+  //       //   this.http.get('http://api.openweathermap.org/data/2.5/weather?q=' + this.cityName + '&appid=2e24ce1a5691ac298a7d48bb0d69efc9', options).subscribe( response => {
+  //         this.http.get('http://ec2-52-59-160-108.eu-central-1.compute.amazonaws.com/api/single-weather?city=' + this.cityName, options).subscribe( response => {
+  //           this.data = response.json();
+  //           console.log('data', this.data);
+  //           this.weatherImag = './src/app/images/'+ this.data.weather[0].icon + '.png';
+  //           this.temp = this.data.main.temp;
+  //       });
   //         this.weather = true;
   //         this.inputValue = input;
   //     } else if (input = ' ') {
@@ -79,8 +71,56 @@ constructor(http: Http) {
   //     }
   // }
 
+  getWeather(input: string) {
+      if (input) {
+          this.cityName = input;
+          this.weatherImag = './src/app/images/'+ this.data.weather[0].icon + '.png';
+          this.temp = this.data.main.temp;
+          console.log('data', this.data);
+          this.weather = true;
+          this.inputValue = input;
+      } else if (input = ' ') {
+          console.log('nothing choosen');
+      }
+      this.getForecast();
+  }
+  getForecast() {
+      this.showForecast = !this.showForecast;
+      if (this.showForecast == true) {
+            this.fDays.length = 0;
+            this.forecustButton = 'Hide Forecast';
+      } else {
+          this.forecustButton = 'Show Forecast';
+      }
+
+      var monthNames = [ " ", "Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Augt", "Sep", "Oct", "Nov", "Dec" ];
+
+      this.http.get('./src/app/images/forecast.json')
+        .subscribe(res => this.forecast = res.json());
+        for (var i = 0; i < this.forecast.list.length; i+=8) {
+            var date = this.forecast.list[i].dt_txt.slice(8, 11);
+            var monthIndex = this.forecast.list[i].dt_txt.slice(5, 7);
+            if (monthIndex < 10) {
+                monthIndex = this.forecast.list[i].dt_txt.slice(6, 7);
+            } else if (monthIndex >= 10) {
+                monthIndex = this.forecast.list[i].dt_txt.slice(5, 7);
+            }
+            var day = {
+                date: date + ' ' + monthNames[monthIndex],
+                temp: this.forecast.list[i].main.temp.toFixed(0),
+                icon: './src/app/images/' + this.forecast.list[i].weather[0].icon + '.png'
+            }
+            this.fDay = day;
+            var days = this.fDays.push(day);
+        }
+  }
+
+
+
   closeWeather() {
       this.weather = false;
+      this.fDays = [];
+      this.showForecast = true;
       this.inputValue = '';
   }
 
